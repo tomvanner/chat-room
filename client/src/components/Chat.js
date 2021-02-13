d35cbd9c96e399c6917bae19fa508a7a98cf6e6f
Tom <thomasvanner98@gmail.com>
2021-02-13 16:52:51 +0000

    callback fix

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,25 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import { 
+    Box,
+    Button,
+    Grid,
+    makeStyles,
+    TextField,
+} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    messageBox: {
+        marginRight: 5
+    },
+    messageButton: {
+        position: "relative",
+        top: 10
+    },
+}));
 
 let socket;
 
 const Chat = ({ location }) => {
+    const classes = useStyles();
     const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const [room, setRoom] = useState('');
+
+
     const ENDPOINT = 'localhost:5000';
 
+    // Connect user to room
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
         socket = io(ENDPOINT);
         setName(name);
         setRoom(room);
-
-        socket.emit('join', { name, room });
+        
+        socket.emit('join', { name, room }, () => {});
+        
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        };        
     }, [ENDPOINT, location.search]);
+    
+    // Receive a message
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    };
+
+    console.log(message, messages)
 
     return (
-        <h1>Chat</h1>
+        <Box>
+            <TextField 
+                label="Message" 
+                variant="outlined" 
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+                className={classes.messageBox}
+            />
+            <Button 
+                variant="contained" 
+                color="primary" 
+                type="submit"
+                className={classes.messageButton}
+            >
+                Send
+            </Button>
+        </Box>
     );
 };
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {
+    addUser,
+    getUser,
+    getUsersInRoom,
+    removeUser,
+} = require('./users.js');
+
 const PORT = 5000 || process.env.PORT;
 
 const router = require('./router');
@@ -12,12 +19,54 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    socket.on('join', ({ name, room }) => {
-        console.log(name, room);
+    socket.on('join', ({ name, room, callback }) => {
+        const { error, user } =  addUser({ id: socket.id, name, room });
+        if(error) {
+            return callback(error);
+        }
+        
+        const welcomeMessage = `${name} has joined.`;
+        socket.emit(
+            'message', 
+            { user: 'admin', text:  welcomeMessage}
+        );
+        socket.broadcast.to(user.room).emit(
+            'message', 
+            { user: 'admin', text: welcomeMessage}
+        );
+
+        socket.join(user.room);
+        
+        io.to(user.room).emit(
+            'roomData', 
+            { room: user.room, users: getUsersInRoom(user.room) }
+        )
+
+        if(callback) {
+            callback();
+        }
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit(
+            'message',
+            { user: user.name, text: message }
+        )
+        io.to(user.room).emit(
+            'roomData',
+            { room: user.room, users: getUsersInRoom(user.room) }
+        )
+
+        callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('disconnect')
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
+        }
     });
 });
 
